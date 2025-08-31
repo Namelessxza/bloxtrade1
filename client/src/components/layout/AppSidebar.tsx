98bd5ab5174a1fa5b4d5bd553e4feaeabdbe3a03
Namelessxza <iaintgotnobread@gmail.com>
2025-08-31 07:19:02 +0000

    Restored to '57f98da886b06d48b106a96415122ee2b67c3eea'
    Replit-Restored-To: 57f98da886b06d48b106a96415122ee2b67c3eea

--- client/src/components/layout/AppSidebar.tsx
+++ client/src/components/layout/AppSidebar.tsx
@@ -89,10 +89,7 @@ export const AppSidebar: React.FC = () => {
         </div>
       </div>
       {/* Navigation Items with Container */}
-      <div className="flex-1 px-1 bg-[#0C1321] relative sidebar-scroll" style={{
-        minHeight: '0',
-        maxHeight: '100%'
-      }}>
+      <SidebarScrollArea className="flex-1 px-1 bg-[#0C1321] relative">
         <div className="p-3 space-y-1.5 rounded-xl ml-0 mr-1 mt-2 bg-[#09101D]">
           {currentCategories.map((item) => (
             <NavItem
@@ -103,9 +100,9 @@ export const AppSidebar: React.FC = () => {
             />
           ))}
         </div>
-      </div>
+      </SidebarScrollArea>
       {/* Bottom Actions */}
-      <div className="flex-shrink-0 p-3 space-y-1.5 rounded-xl ml-0 mr-1 mb-2 bg-[#09101D]">
+      <div className="p-3 space-y-1.5 rounded-xl ml-0 mr-1 mb-2 bg-[#09101D]">
         <NavItem
           item={{ id: "help", label: "Help Center", icon: HelpCircle }}
           selected={false}
